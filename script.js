390aeab340ec8e815383e8cb04b30253ab8f2bae
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2021-12-10 17:57:52 -0600

    add for loop, moment.js, and connect class for color changing times

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-var currentDay = 
+var currentDateEl = $("#currentDay");
+var currentDate = moment().format("LL"); 
+var textAreaEl = $("textarea");
+var currentHour = moment().format("H");
+
+//Current date on page
+currentDateEl.text(currentDate);
+
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings("textarea").val();
+    var time = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(time, text)
+});
+
+for( var i=9; i <= 17; i++) {
+$("#" + i).val(localStorage.getItem(i));
+};
+
+$("textarea").each(function() {
+    var textareaHour = parseInt($(this).attr("id"));
+    currentHour = parseInt(currentHour);
+    console.log(currentHour)
+    if (textareaHour < currentHour) {
+        $(this).addClass("past")
+    }
+    else if (textareaHour === currentHour) {
+        $(this).addClass("present")
+    }
+    else {
+        $(this).addClass("future")
+    }
+});
